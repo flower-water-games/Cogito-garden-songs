822969a3a66b40086cc6b8501d460421a6981b7e
Carlos Michael <carlosmichaeldev@gmail.com>
2024-02-28 00:40:03 -0500

    doing things with watering cans

--- COGITO/Components/PickupComponent.gd
+++ COGITO/Components/PickupComponent.gd
@@ -3,13 +3,18 @@ class_name PickupComponent
 
 @export var slot_data : InventorySlotPD
 
-var player_interaction_component
+var player_interaction_component : PlayerInteractionComponent
+
+@export var auto_equip = true
 
 func interact(_player_interaction_component):
 			
 	if _player_interaction_component.get_parent().inventory_data.pick_up_slot_data(slot_data):
 		_player_interaction_component.send_hint(slot_data.inventory_item.icon, slot_data.inventory_item.name + " added to inventory.")
 		Audio.play_sound(slot_data.inventory_item.sound_pickup)
+		if auto_equip:
+			_player_interaction_component.equip_wieldable(slot_data.inventory_item)
+
 		self.get_parent().queue_free()
 
 
--- FRESH-FLOWERS/Scripts/Wieldables/Wieldable_WateringCan.gd
+++ FRESH-FLOWERS/Scripts/Wieldables/Wieldable_WateringCan.gd
@@ -7,9 +7,9 @@ var current_water_level: float
 @onready var water_stream: GPUParticles3D = %WaterStream
 @onready var interaction_raycast: RayCast3D = %WateringRaycast
 
+@onready var target_rotation_z = rotation_degrees.z + 10
 func action_primary(item, is_released):
 	item_reference = item
-	print("Watering Can: Primary action")
 	if is_watering:
 		stop_watering()
 	else:
@@ -54,22 +54,39 @@ func check_for_waterable_surface():
 			watering_target = null
 			
 
-var original_rotation: Vector3
+@onready var original_rotation: Vector3 = rotation_degrees
 
 func start_watering():
 	if not is_watering and current_water_level > 0:
 		is_watering = true
-		original_rotation = rotation_degrees
 		water_stream.emitting = true
-		rotate_x(-45)
+		# Tween the rotation
+		tween_down()
 		watering_timer.start()
+		print("Watering Can: Started watering")
+
+
+func tween_down():
+	var tween = create_tween()
+	tween.connect("tween_all_completed", on_tween_complete)
+	tween.tween_property(self, "rotation_degrees:z", target_rotation_z, .5)
+
+func tween_up():
+	var tween = create_tween()
+	tween.connect("tween_all_completed", on_tween_complete)
+	tween.tween_property(self, "rotation_degrees:z", original_rotation.z, .5)
+	
+
+func on_tween_complete(tween):
+	tween.queue_free()
 
 func stop_watering():
 	if is_watering:
 		is_watering = false
-		rotation_degrees = original_rotation
+		tween_up()
 		water_stream.emitting = false
 		watering_timer.stop()
+		print("Watering Can: Stopped watering")
 
 func _on_watering_timer_timeout():
 	use_water()
